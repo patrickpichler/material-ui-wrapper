ea3719092d0728085dc81aeff2ead622ec660dba
Patrick Pichler <patri.pichler@gmail.com>
2018-09-16 15:28:40 +0200

    Remove testing folder

--- test/material_ui_wrapper/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns material-ui-wrapper.core-test
-  (:require [clojure.test :refer :all]
-            [material-ui-wrapper.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (some? ((ns-publics 'material-ui-wrapper.core)) "Button"))))
